1c49549b368686a88c564fe1a2a3c7b6d713dd00
I.Z.I <dark_avenger63@mail.ru>
2022-08-17 13:49:14 +0400

    .clang-format .gitignore

--- Source/ForceStrike/Private/FirstPawn.cpp
+++ Source/ForceStrike/Private/FirstPawn.cpp
@@ -1,92 +1,78 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "FirstPawn.h"
-
-
 
 // Sets default values
 AFirstPawn::AFirstPawn()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    // Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 
-	
-	PrimaryActorTick.bCanEverTick = true;
-	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
-	RootComponent = Mesh;
-	
-	
-	//Create Camera
-	SpringArm = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
-	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
-	Camera->SetRelativeLocation(FVector(-120.0f, 0.0f, 214.0f));
-	Camera->SetRelativeRotation(FRotator(-21.0f, 0.0f, 0.0f));
-	Camera->bUsePawnControlRotation = true;
-	
-	//Create SkeletalMesh
-	SkeletalMesh = CreateDefaultSubobject<USkeletalMeshComponent>("SkeletanMesh");
-	SkeletalMesh->SetRelativeLocation(FVector(200.0f, 0.0f, 0.0f ));
-	SkeletalMesh->SetRelativeRotation(FRotator(0.0f, -90.0f, 0.0f));	
-	
-	bUseControllerRotationPitch = true;
-	bUseControllerRotationYaw = true;
+    PrimaryActorTick.bCanEverTick = true;
+    Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+    RootComponent = Mesh;
 
+    // Create Camera
+    SpringArm = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
+    Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+    Camera->SetRelativeLocation(FVector(-120.0f, 0.0f, 214.0f));
+    Camera->SetRelativeRotation(FRotator(-21.0f, 0.0f, 0.0f));
+    Camera->bUsePawnControlRotation = true;
 
-	SpringArm->SetupAttachment(Mesh);
-	SkeletalMesh->SetupAttachment(SpringArm);
-	Camera->SetupAttachment(SpringArm);
-	
+    // Create SkeletalMesh
+    SkeletalMesh = CreateDefaultSubobject<USkeletalMeshComponent>("SkeletanMesh");
+    SkeletalMesh->SetRelativeLocation(FVector(200.0f, 0.0f, 0.0f));
+    SkeletalMesh->SetRelativeRotation(FRotator(0.0f, -90.0f, 0.0f));
 
-	PawnMovementComponent = CreateDefaultSubobject<UFloatingPawnMovement>("PawnMovementComponent");
+    bUseControllerRotationPitch = true;
+    bUseControllerRotationYaw = true;
 
+    SpringArm->SetupAttachment(Mesh);
+    SkeletalMesh->SetupAttachment(SpringArm);
+    Camera->SetupAttachment(SpringArm);
 
-
+    PawnMovementComponent = CreateDefaultSubobject<UFloatingPawnMovement>("PawnMovementComponent");
 }
 
 // Called when the game starts or when spawned
 void AFirstPawn::BeginPlay()
 {
-	Super::BeginPlay();
-	
+    Super::BeginPlay();
 }
 
 // Called every frame
 void AFirstPawn::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
-
+    Super::Tick(DeltaTime);
 }
-
-
 
 // Called to bind functionality to input
-void AFirstPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void AFirstPawn::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-	PlayerInputComponent->BindAxis("MoveForward", this, &AFirstPawn::MoveForward);
-	PlayerInputComponent->BindAxis("MoveLeft", this, &AFirstPawn::MoveLeft);
-	PlayerInputComponent->BindAxis("MouseUD", this, &AFirstPawn::MouseUD);
-	PlayerInputComponent->BindAxis("MouseLR", this, &AFirstPawn::MouseLR);
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+    PlayerInputComponent->BindAxis("MoveForward", this, &AFirstPawn::MoveForward);
+    PlayerInputComponent->BindAxis("MoveLeft", this, &AFirstPawn::MoveLeft);
+    PlayerInputComponent->BindAxis("MouseUD", this, &AFirstPawn::MouseUD);
+    PlayerInputComponent->BindAxis("MouseLR", this, &AFirstPawn::MouseLR);
 }
 
-void AFirstPawn::MoveForward(float Axis) 
+void AFirstPawn::MoveForward(float Axis)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Move forward: %f"), Axis)
-	AddMovementInput(GetActorForwardVector(), Axis, false);
+    UE_LOG(LogTemp, Warning, TEXT("Move forward: %f"), Axis)
+    AddMovementInput(GetActorForwardVector(), Axis, false);
 }
 
-void AFirstPawn::MoveLeft(float Axis) 
+void AFirstPawn::MoveLeft(float Axis)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Move left/right: %f"), Axis)
-	AddMovementInput(GetActorRightVector(), Axis, false);
+    UE_LOG(LogTemp, Warning, TEXT("Move left/right: %f"), Axis)
+    AddMovementInput(GetActorRightVector(), Axis, false);
 }
 
 void AFirstPawn::MouseUD(float Axis)
 {
-	AddControllerPitchInput(-Axis);
+    AddControllerPitchInput(-Axis);
 }
 
 void AFirstPawn::MouseLR(float Axis)
 {
-	AddControllerYawInput(Axis);
+    AddControllerYawInput(Axis);
 }
--- Source/ForceStrike/Public/FirstPawn.h
+++ Source/ForceStrike/Public/FirstPawn.h
@@ -2,67 +2,62 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
-#include "Components/StaticMeshComponent.h"
-#include "Components/CapsuleComponent.h"
-#include "GameFramework/SpringArmComponent.h"
-#include "GameFramework/FloatingPawnMovement.h"
-#include "Components/SkeletalMeshComponent.h"
 #include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
 #include "Components/InputComponent.h"
-#include "GameFramework/Pawn.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "CoreMinimal.h"
 #include "FirstPawn.generated.h"
+#include "GameFramework/FloatingPawnMovement.h"
+#include "GameFramework/Pawn.h"
+#include "GameFramework/SpringArmComponent.h"
 
-//TEST12
+// TEST12
 
 UCLASS()
 class FORCESTRIKE_API AFirstPawn : public APawn
 {
-	GENERATED_BODY()
-		
+    GENERATED_BODY()
 
-public:
-	// Sets default values for this pawn's properties
-	AFirstPawn();
-	
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-	
-	UPROPERTY(EditAnywhere)
-	UStaticMeshComponent* Mesh;
+  public:
+    // Sets default values for this pawn's properties
+    AFirstPawn();
 
-	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
-	USpringArmComponent* SpringArm;
+  protected:
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
 
-	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
-	UCameraComponent* Camera;
-	
-	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "Components")
-	class UFloatingPawnMovement* PawnMovementComponent;
+    UPROPERTY(EditAnywhere)
+    UStaticMeshComponent *Mesh;
 
-	UPROPERTY(BlueprintReadWrite, EditAnyWhere)
-	USkeletalMeshComponent* SkeletalMesh;
-	
-	
+    UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
+    USpringArmComponent *SpringArm;
 
-public:	
+    UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
+    UCameraComponent *Camera;
 
-	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-	
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-	
-	//Control *mouse/keyboard*
-	void MoveForward(float Axis);
-	void MoveLeft(float Axis);
+    UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "Components")
+    class UFloatingPawnMovement *PawnMovementComponent;
 
-	void MouseUD(float Axis);
-	void MouseLR(float Axis);
+    UPROPERTY(BlueprintReadWrite, EditAnyWhere)
+    USkeletalMeshComponent *SkeletalMesh;
 
+    /*UPROPERTY(EditAnywhere)
+    UCapsuleComponent* CapsuleComponent;
+*/
 
+  public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
 
-	
+    // Called to bind functionality to input
+    virtual void SetupPlayerInputComponent(class UInputComponent *PlayerInputComponent) override;
+
+    // Control *mouse/keyboard*
+    void MoveForward(float Axis);
+    void MoveLeft(float Axis);
+
+    void MouseUD(float Axis);
+    void MouseLR(float Axis);
 };
